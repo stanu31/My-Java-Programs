0f4ffd9521d2a0173170e529f8e3f5dff0ceca04
stanu31 <167893730+stanu31@users.noreply.github.com>
2024-04-27 20:22:38 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,39 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+      String s = "Tanugojo";
+      String t="shuklasataru";
+      int l=s.length();
+      int w=t.length();
+      System.out.println(s.concat(t));
+     System.out.println(l);
+     System.out.println(w);
+      System.out.println(s.trim());
+      System.out.println(s.toUpperCase());
+      	 	 System.out.println(s.toUpperCase().charAt(2));
+     System.out.println(t.toUpperCase());
+     System.out.println(s.equals(t));
+          System.out.println(s.equalsIgnoreCase(t));
+          System.out.println(s.lastIndexOf("anu"));
+System.out.println(s.substring(5));
+System.out.println(t.substring(0,5));
+System.out.println(t.charAt(3));
+System.out.println(s.indexOf(“go”,3));
+System.out.println(s.compareTo(t));
+System.out.println(s.compareToIgnoreCase(t));
+System.out.println(s.startsWith(t));
+char []ch=s.toCharArray();
+System.out.println(s.contains(t));
+
+
+
+
+
+     
+     
+
+    
+    }
+}
